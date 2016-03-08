fd7a6378310b35d1333f0537f00eb5cb1551ddd7
Renzo Frigato <rfrigato@stanford.edu>
2016-03-08 11:46:01 -0800

    use container id for snapshot analytics

--- api/dao/analytics.py
+++ api/dao/analytics.py
@@ -2,22 +2,25 @@ from .. import config
 
 log = config.log
 
-def add(analytics_type, user_id, user_site, timestamp):
+def add(analytics_type, container_id, user_id, user_site, timestamp):
     view = dict(
         analytics_type=analytics_type,
+        container_id=container_id,
         user_id=user_id,
         user_site=user_site,
         timestamp=timestamp
     )
     config.db.analytics.insert_one(view)
 
-def get(analytics_type, user_id, user_site, start_date, end_date, count, limit):
+def get(analytics_type, container_id, user_id, user_site, start_date, end_date, count, limit):
     query = {}
     if user_id:
         query['user_id'] = user_id
         query['user_site'] = user_site
     if analytics_type:
         query['analytics_type'] = analytics_type
+    if container_id:
+        query['container_id'] = container_id
     date_query = {}
     if start_date:
         date_query['$gte'] = start_date
--- api/handlers/analyticshandler.py
+++ api/handlers/analyticshandler.py
@@ -14,7 +14,7 @@ class AnalyticsHandler(base.RequestHandler):
     def __init__(self, request=None, response=None):
         super(AnalyticsHandler, self).__init__(request, response)
 
-    def get(self, cont_name, **kwargs):
+    def get(self, cont_name, cid, **kwargs):
         start_date = self.get_param('start_date')
         if start_date:
             year, month, day = [int(i) for i in start_date.split('-')]
@@ -34,6 +34,7 @@ class AnalyticsHandler(base.RequestHandler):
         limit = 10 if self.get_param('limit') is None else int(self.get_param('limit'))
         result = analytics.get(
             self.get_param('type'),
+            cid,
             user_id, user_site,
             start_date,
             end_date,
@@ -42,12 +43,13 @@ class AnalyticsHandler(base.RequestHandler):
         )
         return result
 
-    def post(self, cont_name, **kwargs):
+    def post(self, cont_name, cid, **kwargs):
         analytic_type = self.get_param('type')
         if not analytic_type:
             self.abort(400, 'missing view type in request')
         result = analytics.add(
             analytic_type,
+            cid,
             self.uid,
             self.user_site,
             datetime.datetime.utcnow()
