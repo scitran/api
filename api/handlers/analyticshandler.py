836ff8049786db7fd359e80d6fcd4b9270b2ac34
Renzo Frigato <rfrigato@stanford.edu>
2016-03-08 16:22:43 -0800

    fix logic to get analytics
    works on #3

--- api/handlers/analyticshandler.py
+++ api/handlers/analyticshandler.py
@@ -17,19 +17,28 @@ class AnalyticsHandler(base.RequestHandler):
     def get(self, cont_name, cid, **kwargs):
         start_date = self.get_param('start_date')
         if start_date:
-            year, month, day = [int(i) for i in start_date.split('-')]
-            start_date = datetime.datetime(year, month, day)
+            try:
+                year, month, day = [int(i) for i in start_date.split('-')]
+                start_date = datetime.datetime(year, month, day)
+            except ValueError:
+                self.abort(400, 'date format is {year}-{month}-{day}')
         end_date = self.get_param('end_date')
         if end_date:
-            year, month, day = [int(i) for i in end_date.split('-')]
-            end_date = datetime.datetime(year, month, day + 1)
+            try:
+                year, month, day = [int(i) for i in end_date.split('-')]
+                end_date = datetime.datetime(year, month, day) + datetime.timedelta(days=1)
+            except ValueError:
+                self.abort(400, 'date format is {year}-{month}-{day}')
         if self.superuser_request:
             user_id = self.get_param('user_id')
             user_site = self.get_param('user_site') or config.get_item('site', 'id')
         elif not self.get_param('user_id'):
+            user_id = None
+            user_site = None
+        elif self.get_param('user_id') == self.uid or self.get_param('site') == self.user_site:
             user_id = self.uid
             user_site = self.user_site
-        elif self.get_param('user_id') != self.uid or self.get_param('site') != self.user_site:
+        else:
             self.abort(400, 'user must be admin to perform the request')
         limit = 10 if self.get_param('limit') is None else int(self.get_param('limit'))
         result = analytics.get(
@@ -38,7 +47,7 @@ class AnalyticsHandler(base.RequestHandler):
             user_id, user_site,
             start_date,
             end_date,
-            self.get_param('count'),
+            self.is_true('count'),
             limit
         )
         return result
