b17b754227c46dd315327d68e706bbb0b924029f
Colton Leekley-Winslow <coltonlw@flywheel.io>
2016-08-10 17:36:55 -0500

    run abao as root user

--- test/integration_tests/abao_test_hooks.js
+++ test/integration_tests/abao_test_hooks.js
@@ -2,7 +2,8 @@ var hooks = require('hooks');
 
 hooks.beforeEach(function (test, done) {
     test.request.query = {
-      user: 'admin@user.com'
+      user: 'admin@user.com',
+      root: 'true'
     };
     done();
 });
