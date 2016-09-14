55fed795cff7f3484a81d62ec000c610800ef826
Colton Leekley-Winslow <coltonlw@flywheel.io>
2016-09-14 12:50:36 -0500

    Use ubuntu install script for Dockerfile

--- test/bin/run-tests-docker.sh
+++ test/bin/run-tests-docker.sh
@@ -4,9 +4,40 @@ set -e
 unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )/../.."
 
-IMAGE_NAME_SCITRAN_CORE=scitran-core
+IMAGE_NAME_SCITRAN_CORE=${IMAGE_NAME_SCITRAN_CORE:-"scitran-core"}
 IMAGE_NAME_MONGO=mongo
-CONTAINER_NAME_MONGO=some-mongo
+CONTAINER_NAME_MONGO=scitran-core-test-mongo
+
+
+USAGE="
+    Run scitran-core tests using docker
+\n
+    Usage:\n
+    \n
+    --help: print help and exit\n
+    -r, --rebuild-image: Rebuild scitran-core base image\n
+
+"
+
+while [ "$#" -gt 0 ]; do
+    key="$1"
+    case $key in
+        --help)
+        echo $USAGE >&2
+        exit 1
+        ;;
+        -r|--rebuild-image)
+        docker build -t "$IMAGE_NAME_SCITRAN_CORE" .
+        ;;
+        *)
+        echo "Invalid option: $key" >&2
+        echo $USAGE >&2
+        exit 1
+        ;;
+    esac
+    shift
+done
+
 
 clean_up () {
   # Stop and remove mongo container
@@ -17,21 +48,19 @@ trap clean_up EXIT
 
 # Sub-shell the test steps to make the functionality of the trap execution explicit
 (
-# Build the docker image
-docker build -t "$IMAGE_NAME_SCITRAN_CORE" .
-
 # Launch Mongo isinstance
 docker run --name "$CONTAINER_NAME_MONGO" -d "$IMAGE_NAME_MONGO"
 
 # Execute tests
 docker run \
   --rm \
-  --name scitran-core-tester \
+  --name scitran-core-test-uwsgi \
   -e "SCITRAN_PERSISTENT_DB_URI=mongodb://$CONTAINER_NAME_MONGO:27017/scitran" \
   --link "$CONTAINER_NAME_MONGO" \
   -v $(pwd):/var/scitran/code/api \
   --entrypoint bash \
   "$IMAGE_NAME_SCITRAN_CORE" \
+    /var/scitran/code/api/test/bin/setup-integration-tests-ubuntu.sh && \
     /var/scitran/code/api/test/bin/run-tests-ubuntu.sh
 
 )
