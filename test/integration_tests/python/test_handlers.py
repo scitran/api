e07bbf6dfde3f1c2a40ef835942b3413ba8b6dd7
Ambrus Simon <ambrussimon@invenshure.com>
2017-04-13 19:30:41 +0200

    add test_reporthandler

--- test/integration_tests/python/test_handlers.py
+++ test/integration_tests/python/test_handlers.py
@@ -12,3 +12,28 @@ def test_schemahandler(as_public):
     assert r.ok
     schema = r.json()
     assert all(attr in schema['definitions'] for attr in ('_id', 'firstname', 'lastname'))
+
+
+def test_reporthandler(data_builder, randstr, as_admin, as_user):
+    group_name = randstr()
+    group = data_builder.create_group(name=group_name)
+    project = data_builder.create_project()
+    session = data_builder.create_session()
+
+    # try to get site report as non-admin
+    r = as_user.get('/report/site')
+    assert r.status_code == 403
+
+    # get site report
+    r = as_admin.get('/report/site')
+    assert r.ok
+
+    site_report = r.json()
+    group_report = next((g for g in site_report['groups'] if g['name'] == group_name), None)
+    assert group_report is not None
+    assert group_report['project_count'] == 1
+    assert group_report['session_count'] == 1
+
+    # try to get project report w/o perms
+    r = as_user.get('/report/project', params={'projects': project})
+    assert r.status_code == 403
